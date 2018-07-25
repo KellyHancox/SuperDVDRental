27111c9ac005e7dca18f6e87eeec6ea501324ef0
Kelly Hancox <kellyhancox@Kelly-Hancox-Rules.local>
2018-07-25 15:55:51 -0400

    Changed enum comments

--- PlayerType.java
+++ PlayerType.java
@@ -1,5 +1,9 @@
 package project4;
 
+/*****************************************************************
+ * These are the values for the types of game systems. They are
+ * in the Jcombobox
+ *****************************************************************/
 public enum PlayerType {
 	Xbox360, XBox1, PS4, WiiU, NintendoSwitch
 }
